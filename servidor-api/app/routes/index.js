909bb5536d7ef64bfa5464edd298fb4bf74c7645
eduvinagre <eduvinagre@gmail.com>
2023-06-28 17:41:34 -0300

    update

--- servidor-api/app/routes/index.js
+++ servidor-api/app/routes/index.js
@@ -1,4 +1,4 @@
-/* Simple Code made just to give a service to the application para fornecer o serviço para a aplicação */
+/* Simple Code made just to give a service to the application para fornecer o serviço para a  */
 
 var api = require("../api");
 
